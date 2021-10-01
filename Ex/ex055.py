f0d4daced022f0c46cb6ca28b4ee28762c83be02
Rafael <rafaeloliveiracds@hotmail.com>
2021-10-01 01:26:08 -0300

    Ex

--- Ex/ex055.py
+++ Ex/ex055.py
@@ -1,4 +1,15 @@
 #mostre o maior e o menor entre 5 valores
-for e in range(0,5):
-    valor = int(input('Digite um valor: '))
-    
+maior = 0
+menor = 0
+for v in range(1,6):
+    valor = float(input('Digite um valor: '))
+    if v == 1:
+        maior = valor
+        menor = valor
+    else:
+        if valor > maior:
+            maior = valor
+        if valor < menor:
+            menor = valor
+print('O maior valor é {}'.format(maior))
+print('O menor valor é {}'.format(menor))
--- Ex/ex056.py
+++ Ex/ex056.py
@@ -1,1 +1,26 @@
 #Seletor
+pessoas = 0
+idadeMedia = 0
+velho = ['', 0]
+menorde20 = 0
+for p in range(0,4):
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip().lower()
+    print('-='*20)
+    pessoas += 1
+    idadeMedia += idade
+    if sexo == 'm':
+        if idade > velho[1]:
+            velho = [nome, idade]
+    if sexo == 'f':
+        if idade < 20:
+            menorde20 += 1
+        
+        
+idadeMedia = idadeMedia / pessoas
+print('A média de idade do grupo é {:.0f}'.format(idadeMedia))
+print('O Homem mais velho é {} e ele tem {}'.format(velho[0], velho[1]))
+print('Há {} mulheres menores de 20'.format(menorde20))
+
+
